6bc6d4813f46529a24fac7f7c345d10dd79d14fe
Emanuel de Jong <44051853+killbottt@users.noreply.github.com>
2020-02-11 22:03:39 +0100

    save cuz I'm gon sleep. gn world

---
+++ settings/elements/Cs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace elements
+{
+    public class Cs
+    {
+
+    }
+}
--- settings/settings/MainWindow.xaml.cs
+++ settings/settings/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 using elements;
 
 namespace settings
@@ -32,9 +33,9 @@ namespace settings
         {
             ImagesImage n1 = new ImagesImage { name = "n1", path = "6K/note1.png" };
             ImagesImage n1t = new ImagesImage { name = "n1t", path = "6K/note1tail.png" };
-            Keymode km_10K = new Keymode { name = "DJMAX 10K", playfield = "playfield10K.json", cses = new object[] { 0.5, 0, 0, 0, "h" }, images = { n1, n1t } };
+            Keymode km_10K = new Keymode { name = "DJMAX 10K", playfield = "playfield10K.json", cses = new object[] { 0.5, 0, 0, 0, "h" }, images = new List<ImagesImage> { n1, n1t } };
 
-
+            debug.Text = JsonConvert.SerializeObject(km_10K);
         }
     }
 }
