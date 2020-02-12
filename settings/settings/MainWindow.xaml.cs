0b161cb5d6db6264d006c84f07ec2c57ca77dd01
Emanuel de Jong <44051853+killbottt@users.noreply.github.com>
2020-02-12 21:12:11 +0100

    mainwindow cs cleanup

--- settings/settings/MainWindow.xaml.cs
+++ settings/settings/MainWindow.xaml.cs
@@ -47,40 +47,53 @@ namespace settings
 
         private void GenerateJSON(object sender, RoutedEventArgs e)
         {
-            Cs cs = new Cs { x = 0.5, y = 0, a = 0, b = 0, binding = eBinding.h };
+            string name = "DJMAX 10K";
 
-            Image n1 = new Image { name = "n1", path = "6K/note1.png", layer = Layer };
-            Image n1t = new Image { name = "n1t", path = "6K/note1tail.png", layer = Layer, blendAlphaMode = eBlendAlphaMode.alphamultiply };
+            string playfield = "playfield10K.json";
 
-            Dictionary<string, NoteComponent> measure1 = new Dictionary<string, NoteComponent>()
+            List<object[]> cses = new List<object[]>()
             {
-                { "Head", new NoteComponent()
-                {
-                    cs = 1,
-                    gc = new Gc {
-                        x = new List<double>() { -0.222 }, y = new List<double>() { 0.699, 1 },
-                        w = new List<double>() { 0.464 }, h = new List<double>() { 0 },
-                        ox = new List<double>() { 0 }, oy = new List<double>() { 0 }
-                    },
-                    sb = new Sb(),
-                    layer = 0,
-                    image = "white"
-                }}
+                new Cs { x = 0.5, y = 0, a = 0, b = 0, binding = eBinding.h }.getCsAsArr()
             };
 
-
-            Keymode km_10K = new Keymode
-            { name = "DJMAX 10K", playfield = "playfield10K.json", cses = new List<object[]> { cs.getCsAsArr() }, 
-                images = new List<Image> { n1, n1t }, notes = new Dictionary<string, Dictionary<string, NoteComponent>>() { { "measure1:ShortNote", measure1 } }
+            List<Image> images = new List<Image>()
+            {
+                new Image { name = "n1", path = "6K/note1.png", layer = Layer },
+                new Image { name = "n1t", path = "6K/note1tail.png", layer = Layer, blendAlphaMode = eBlendAlphaMode.alphamultiply }
             };
+
+            Dictionary<string, Dictionary<string, NoteComponent>> notes = new Dictionary<string, Dictionary<string, NoteComponent>>()
+            {
+                { 
+                    "measure1", new Dictionary<string, NoteComponent>()
+                    {
+                        { 
+                            "Head", new NoteComponent()
+                            {
+                                cs = 1,
+                                gc = new Gc {
+                                    x = new List<double>() { -0.222 }, y = new List<double>() { 0.699, 1 },
+                                    w = new List<double>() { 0.464 }, h = new List<double>() { 0 },
+                                    ox = new List<double>() { 0 }, oy = new List<double>() { 0 }
+                                },
+                                layer = Layer,
+                                image = images[0].name
+                            }
+                        } 
+                    }
+                }
+            };
+
+            Keymode km_10K = new Keymode { name = name, playfield = playfield, cses = cses, images = images, notes = notes };
 
             showJSON(km_10K);
         }
 
         public void showJSON(object objJSON)
         {
-            debug.Text = JsonConvert.SerializeObject(objJSON, Formatting.Indented, new JsonSerializerSettings { 
-                NullValueHandling = NullValueHandling.Ignore 
+            debug.Text = JsonConvert.SerializeObject(objJSON, Formatting.Indented, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
             });
         }
     }
