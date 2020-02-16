96a474f073fb7cebf8a4c12b825f224450b7bc90
Emanuel de Jong <44051853+killbottt@users.noreply.github.com>
2020-02-16 14:52:59 +0100

    GetNotes

--- settings/elements/Keymode.cs
+++ settings/elements/Keymode.cs
@@ -36,8 +36,8 @@ namespace elements
                 {
                     string side = i == 1 ? "left" : "right";
                     images.Add(new Image() { name = "fx" + side + "b", path = "resources/8K/fx" + side + "body.png", blendAlphaMode = eBlendAlphaMode.alphamultiply, layer = sLayer.GetAndIncrement() });
-                    images.Add(new Image() { name = "fx" + side + "h", path = "resources/8K/fx" + side + "head.png", blendAlphaMode = eBlendAlphaMode.alphamultiply, layer = sLayer.GetAndIncrement() });
                     images.Add(new Image() { name = "fx" + side + "t", path = "resources/8K/fx" + side + "tail.png", blendAlphaMode = eBlendAlphaMode.alphamultiply, layer = sLayer.GetAndIncrement() });
+                    images.Add(new Image() { name = "fx" + side + "h", path = "resources/8K/fx" + side + "head.png", blendAlphaMode = eBlendAlphaMode.alphamultiply, layer = sLayer.GetAndIncrement() });
                     images.Add(new Image() { name = "fx" + side, path = "resources/8K/fx" + side + ".png", blendAlphaMode = eBlendAlphaMode.alphamultiply, layer = sLayer.GetAndIncrement() });
                 }
             }
@@ -50,8 +50,8 @@ namespace elements
                     {
                         string side = j == 1 ? "left" : "right";
                         images.Add(new Image() { name = "fx" + fxCount + side + "b", path = "resources/10K/fx" + fxCount + side + "body.png", blendAlphaMode = eBlendAlphaMode.alphamultiply, layer = sLayer.GetAndIncrement() });
-                        images.Add(new Image() { name = "fx" + fxCount + side + "h", path = "resources/10K/fx" + fxCount + side + "head.png", blendAlphaMode = eBlendAlphaMode.alphamultiply, layer = sLayer.GetAndIncrement() });
                         images.Add(new Image() { name = "fx" + fxCount + side + "t", path = "resources/10K/fx" + fxCount + side + "tail.png", blendAlphaMode = eBlendAlphaMode.alphamultiply, layer = sLayer.GetAndIncrement() });
+                        images.Add(new Image() { name = "fx" + fxCount + side + "h", path = "resources/10K/fx" + fxCount + side + "head.png", blendAlphaMode = eBlendAlphaMode.alphamultiply, layer = sLayer.GetAndIncrement() });
                         images.Add(new Image() { name = "fx" + fxCount + side, path = "resources/10K/fx" + fxCount + side + ".png", blendAlphaMode = eBlendAlphaMode.alphamultiply, layer = sLayer.GetAndIncrement() });
                     }
                 }
@@ -61,8 +61,8 @@ namespace elements
             if (sidetracks)
             {
                 images.Add(new Image() { name = "stb", path = "resources/stbody.png", layer = sLayer.GetAndIncrement() });
-                images.Add(new Image() { name = "sth", path = "resources/sthead.png", layer = sLayer.GetAndIncrement() });
                 images.Add(new Image() { name = "stt", path = "resources/sttail.png", layer = sLayer.GetAndIncrement() });
+                images.Add(new Image() { name = "sth", path = "resources/sthead.png", layer = sLayer.GetAndIncrement() });
                 images.Add(new Image() { name = "st", path = "resources/st.png", layer = sLayer.GetAndIncrement() });
             }
 
@@ -74,15 +74,15 @@ namespace elements
             }
             else
             {
-                dir += keymode.ToString() + "K";
+                dir += keymode + "K";
             }
 
             for (int i=1; i <=2; i++)
             {
                 string noteCount = i.ToString();
                 images.Add(new Image() { name = "n" + noteCount + "b", path = dir + "/note" + noteCount + "body.png", layer = sLayer.GetAndIncrement() });
-                images.Add(new Image() { name = "n" + noteCount + "h", path = dir + "/note" + noteCount + "head.png", layer = sLayer.GetAndIncrement() });
                 images.Add(new Image() { name = "n" + noteCount + "t", path = dir + "/note" + noteCount + "tail.png", layer = sLayer.GetAndIncrement() });
+                images.Add(new Image() { name = "n" + noteCount + "h", path = dir + "/note" + noteCount + "head.png", layer = sLayer.GetAndIncrement() });
                 images.Add(new Image() { name = "n" + noteCount, path = dir + "/note" + noteCount + ".png", layer = sLayer.GetAndIncrement() });
             }
 
@@ -94,7 +94,7 @@ namespace elements
         {
             Dictionary<string, Dictionary<string, NoteComponent>> notes = new Dictionary<string, Dictionary<string, NoteComponent>>();
 
-            string dir = "settingsresources/positions/positions" + keymode.ToString() + "K";
+            string dir = "settingsresources/positions/positions" + keymode + "K";
             dir += sidetracks ? "2ST" : "";
             dir += ".json";
             string json = File.ReadAllText(dir);
@@ -112,11 +112,100 @@ namespace elements
                                 w = measure1Pos["w"], h = measure1Pos["h"],
                                 ox = measure1Pos["ox"], oy = measure1Pos["oy"]
                             },
-                            layer = sLayer.GetAndIncrement(),
-                            image = images[0].name
+                            layer = images[sImgIndex.ImgIndex].layer,
+                            image = images[sImgIndex.GetAndIncrement()].name
                         }
                     }
                 };
+
+
+            int loops = keymode >= 6 ? 6 : keymode;
+            for (int i=1; i<= loops; i++)
+            {
+                var keyPos = positions[i];
+
+                var longNotePos = keyPos[eNoteType.LongNote.ToString()];
+                notes["key" + i + ":" + eNoteType.LongNote] =
+                    new Dictionary<string, NoteComponent>() {
+                        {
+                            eNoteComponent.Body.ToString(),
+                            new NoteComponent() {
+                                cs = 1,
+                                gc = new Gc {
+                                    x = longNotePos["Body"]["x"], y = longNotePos["Body"]["y"],
+                                    w = longNotePos["Body"]["w"], h = longNotePos["Body"]["h"],
+                                    ox = longNotePos["Body"]["ox"], oy = longNotePos["Body"]["oy"]
+                                },
+                                layer = images[sImgIndex.ImgIndex].layer,
+                                image = images[sImgIndex.GetAndIncrement()].name
+                            }
+                        },
+                        {
+                            eNoteComponent.Tail.ToString(),
+                            new NoteComponent() {
+                                cs = 1,
+                                gc = new Gc {
+                                    x = longNotePos["Tail"]["x"], y = longNotePos["Tail"]["y"],
+                                    w = longNotePos["Tail"]["w"], h = longNotePos["Tail"]["h"],
+                                    ox = longNotePos["Tail"]["ox"], oy = longNotePos["Tail"]["oy"]
+                                },
+                                layer = images[sImgIndex.ImgIndex].layer,
+                                image = images[sImgIndex.GetAndIncrement()].name
+                            }
+                        },
+                        {
+                            eNoteComponent.Head.ToString(),
+                            new NoteComponent() {
+                                cs = 1,
+                                gc = new Gc {
+                                    x = longNotePos["Head"]["x"], y = longNotePos["Head"]["y"],
+                                    w = longNotePos["Head"]["w"], h = longNotePos["Head"]["h"],
+                                    ox = longNotePos["Head"]["ox"], oy = longNotePos["Head"]["oy"]
+                                },
+                                layer = images[sImgIndex.ImgIndex].layer,
+                                image = images[sImgIndex.GetAndIncrement()].name
+                            }
+                        }
+                    };
+
+                var shortNotePos = keyPos[eNoteType.ShortNote.ToString()];
+                notes["key" + i + ":" + eNoteType.ShortNote] =
+                    new Dictionary<string, NoteComponent>() {
+                        {
+                            eNoteComponent.Head.ToString(),
+                            new NoteComponent() {
+                                cs = 1,
+                                gc = new Gc {
+                                    x = shortNotePos["Head"]["x"], y = shortNotePos["Head"]["y"],
+                                    w = shortNotePos["Head"]["w"], h = shortNotePos["Head"]["h"],
+                                    ox = shortNotePos["Head"]["ox"], oy = shortNotePos["Head"]["oy"]
+                                },
+                                layer = images[sImgIndex.ImgIndex].layer,
+                                image = images[sImgIndex.ImgIndex].name
+                            }
+                        }
+                    };
+
+                var soundNotePos = keyPos[eNoteType.SoundNote.ToString()];
+                notes["key" + i + ":" + eNoteType.SoundNote] =
+                    new Dictionary<string, NoteComponent>() {
+                        {
+                            eNoteComponent.Head.ToString(),
+                            new NoteComponent() {
+                                cs = 1,
+                                gc = new Gc {
+                                    x = soundNotePos["Head"]["x"], y = soundNotePos["Head"]["y"],
+                                    w = soundNotePos["Head"]["w"], h = soundNotePos["Head"]["h"],
+                                    ox = soundNotePos["Head"]["ox"], oy = soundNotePos["Head"]["oy"]
+                                },
+                                layer = images[sImgIndex.ImgIndex].layer,
+                                image = images[sImgIndex.ImgIndex].name
+                            }
+                        }
+                    };
+            }
+
+
 
             return notes;
         }
--- settings/elements/Positions.cs
+++ settings/elements/Positions.cs
@@ -20,5 +20,36 @@ namespace elements
         public Dictionary<string, Dictionary<string, Dictionary<string, List<double>>>> key10 { get; set; }
         public Dictionary<string, Dictionary<string, Dictionary<string, List<double>>>> scratch1 { get; set; }
         public Dictionary<string, Dictionary<string, Dictionary<string, List<double>>>> scratch2 { get; set; }
+
+        public Dictionary<string, Dictionary<string, Dictionary<string, List<double>>>> this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 1:
+                        return key1;
+                    case 2:
+                        return key2;
+                    case 3:
+                        return key3;
+                    case 4:
+                        return key4;
+                    case 5:
+                        return key5;
+                    case 6:
+                        return key6;
+                    case 7:
+                        return key7;
+                    case 8:
+                        return key8;
+                    case 9:
+                        return key9;
+                    case 10:
+                        return key10;
+                }
+                return measure1;
+            }
+        }
     }
 }
