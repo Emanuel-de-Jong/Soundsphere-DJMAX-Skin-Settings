7b56277d694ed3707073fd06ec1fb2c5c067663b
Manuel de Jong <manuel@jaarsma-deboer.nl>
2020-03-16 11:24:24 +0100

    changed git account 4 work. personal still working?

--- settings/settings/MainWindow.xaml.cs
+++ settings/settings/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Controls;
 namespace settings
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml 
     /// </summary>
     public partial class MainWindow : Window
     {
